257cb975d1f86c63dd084e874fc9fa28d19b2fa7
denis <erami.ukr@gmail.com>
2019-03-24 21:04:55 +0200

    Init handlebars

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
+const path = require('path');
 const express = require('express');
 const cfg = require('../config/default');
 
 const app = express();
 
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectoryPath));
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 app.listen(cfg.server.port, () => {
-    console.log("Server listening on " + cfg.server.port);
+    console.log('Server listening on ' + cfg.server.port);
 });
